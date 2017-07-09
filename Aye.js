c15a7f6fe426ce53322368f27c7380679377bc93
GrimmStarrk <Grimmstarrk069@gmail.com>
2017-07-09 00:38:37 -0500

    The Beginning (couple of commands)

---
+++ Aye.js
@@ -0,0 +1,30 @@
+const settings = require("./settings.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+bot.login(settings.TOKEN);
+
+bot.on('ready', function () { 
+  console.log(`Online`); //Bot message saying "Online"\\
+});
+
+bot.on("message", function(msg) {
+    if (msg.author.equals(bot.user)) return;
+    if (!msg.content.startsWith(settings.PREFIX)) return;
+    var args = msg.content.substring(settings.PREFIX.length).split(" ");
+       
+
+    switch (args[0].toLowerCase()) {
+        case "ping": //Ping Command\\
+             msg.channel.send(":ping_pong:"); 
+             break;
+        
+        case "pong": //Pong Command\\
+             msg.channel.send("Wait a minute... aren't I supposed to say that?!");
+             break;
+       
+        case "lookatme": //Lookatme Command\\
+            msg.channel.send("Fuck on me!");
+             break;
+    }   
+});
+             
